dc8792b189995a4b632c75d00f0f5e57fb5ff261
DomKiki <DomKiki@hotmail.com>
2014-08-28 17:20:37 +0200

    Form Class
    Rotation operational

--- Form.scala
+++ Form.scala
@@ -1,81 +1,75 @@
-import java.util.{ArrayList, Random}
+import java.util.Random
+import Array._
 
 class Form {
 
 	private var matrix = Array[Array[Boolean]]()
 	private val max = 7
 
-	def swap: Unit = {
+	def rotate: Unit = {
 
+		var newarr = Array[Array[Boolean]]()
+
+		for (j <- 0 until matrix(0).size) {
+
+			var sub = Array[Boolean]()
+
+			for (i <- (matrix.size-1) to 0 by -1) {	sub = sub ++ Array(matrix(i)(j))}
+
+			newarr = newarr ++ Array(sub)
+
+		}
+
+		matrix = newarr
+				
 	}
 
 	var rnd = Math.round(new Random().nextDouble * (max-1))
-	
+
 	rnd match {
 
-		case 0 => {
-			matrix = Array(Array(true), Array(true), Array(true), Array(true))
-		}
+		case 0 => { matrix = Array(Array(true), Array(true), Array(true), Array(true)) }
 
-		case 1 => {
-			matrix = Array( Array(true, true),
-					Array(true, true) )
-		}
+		case 1 => { matrix = Array( Array(true, true),
+					    Array(true, true) )
+			  }
 
-		case 2 => {
-			matrix = Array( Array(true,  true, true),
-					Array(false, true, false) )
-		}
+		case 2 => { matrix = Array( Array(true, true, true),
+					    Array(false, true, false) )
+			  }
 
-		case 3 => {
-			matrix = Array( Array(true, true,  true),
-					Array(true, false, false) )
-		}
+		case 3 => { matrix = Array( Array(true, true, true),
+				 	    Array(true, false, false) )
+			  }
 
-		case 4 => {
-			matrix = Array( Array(true,  true,  true),
-					Array(false, false, true) )
-		}
+		case 4 => { matrix = Array( Array(true, true, true),
+				            Array(false, false, true) )
+			  }
 
-		case 5 => {
-			matrix = Array( Array(false, true), 
-					Array(true,  true), 
-					Array(true,  false) )
-		}
+		case 5 => { matrix = Array( Array(false, true),
+					    Array(true, true),
+					    Array(true, false) )
+			  }
 
-		case 6 => {
-			matrix = Array( Array(true, false), 
-					Array(true,  true), 
-					Array(false, true) )
-		}
-
-
+		case 6 => { matrix = Array( Array(true, false),
+					    Array(true, true),
+					    Array(false, true) )
+			  }
 	}
 
 	def display: Unit = {
 
 		for (i <- 0 until matrix.size) {
-
 			for (j <- 0 until matrix(i).size) {
 
 				val x = if (matrix(i)(j)) 1 else 0
 				print(" " + x)
 
 			}
-
 			println
-
 		}
-
 		println
 
 	}
-
-}
-
-object FormTest extends App {
-
-	for (i <- 0 until 10) { val test = new Form
-				test.display }
 
 }
