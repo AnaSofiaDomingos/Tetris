cb17cc049bd71e4f4d91be07a8b1d5ffd70b75ac
MelanieSalvi <salvi.melanie94@gmail.com>
2014-09-01 13:32:31 +0200

    Create Grid2.scala

---
+++ Grid2.scala
@@ -0,0 +1,30 @@
+import scala.swing._
+
+object Grid2 extends SimpleSwingApplication{
+	println("Test2")
+	def top = new MainFrame{ 
+		title = "Tetris"
+		preferredSize = new Dimension(600, 800)
+		val bouton = new  Button { text = "MonBouton" }
+		contents = new GridPanel(22,10){
+
+			val j:Int = 22
+			var i:Int = 1
+
+			while(i <= j){
+				contents += new Button()
+      				contents += new Button()
+				contents += new Button()
+				contents += new Button()
+				contents += new Button()
+				contents += new Button()
+				contents += new Button()
+				contents += new Button()
+				contents += new Button()
+				contents += new Button()
+				i = i+1
+      			}
+
+		}
+	}
+}
